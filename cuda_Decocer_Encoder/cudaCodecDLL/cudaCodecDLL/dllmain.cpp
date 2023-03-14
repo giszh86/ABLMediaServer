0a66f89a9b28746b0abaa15a130945c74acd857c
zhengzc <zhichao281@163.com>
2023-03-14 16:08:56 +0800

    +新增了项目文件

---
+++ cuda_Decocer_Encoder/cudaCodecDLL/cudaCodecDLL/dllmain.cpp
@@ -0,0 +1,26 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+#include "cudaCodecDLL.h"
+
+extern "C" cudaCodecDLL_EXPORTSIMPL bool cudaCodec_UnInit();
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		cudaCodec_UnInit();
+		break;
+	}
+	return TRUE;
+}
+
