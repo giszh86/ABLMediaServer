e58a22032d9939878a8bfc91b9b99607a796d20b
zhengzc <zhichao281@163.com>
2023-04-12 16:36:29 +0800

    +修改了server

--- XHNetSDK/server.h
+++ XHNetSDK/server.h
@@ -1,18 +1,61 @@
-#ifndef _SERVER_H_
-#define _SERVER_H_
+#pragma  once 
 
 
 #include "client_manager.h"
 #include "auto_lock.h"
 
-
 #ifdef USE_BOOST
 #include <boost/function.hpp>
+
 #else
 #include <functional>
 #include <memory>
 #endif
+#ifdef USE_BOOST
+class server : public boost::enable_shared_from_this<server>
+{
+public:
+	server(boost::asio::io_context& ioc,
+		boost::asio::ip::tcp::endpoint& ep,
+		accept_callback fnaccept,
+		read_callback fnread,
+		close_callback fnclose,
+		bool autoread);
+	~server();
 
+	int32_t run();
+	void close();
+	NETHANDLE get_id();
+
+private:
+	void start_accept();
+	void handle_accept(client_ptr c, const boost::system::error_code& error);
+	void disconnect_clients();
+
+private:
+	boost::asio::ip::tcp::acceptor m_acceptor;
+	boost::asio::ip::tcp::endpoint m_endpoint;
+	accept_callback m_fnaccept;
+	read_callback m_fnread;
+	close_callback m_fnclose;
+	const bool m_autoread;
+	NETHANDLE m_id;
+};
+typedef boost::shared_ptr<server>  server_ptr;
+
+inline NETHANDLE server::get_id()
+{
+	return m_id;
+}
+
+inline void server::disconnect_clients()
+{
+	client_manager_singleton::get_mutable_instance().pop_server_clients(get_id());
+}
+
+
+
+#else
 
 class server : public std::enable_shared_from_this<server>
 {
@@ -31,7 +74,7 @@ public:
 
 private:
 	void start_accept();
-	void handle_accept(client_ptr c, const asio::error_code& error);
+	void handle_accept(client_ptr c, std::error_code error);
 	void disconnect_clients();
 
 private:
@@ -43,13 +86,7 @@ private:
 	const bool m_autoread;
 	NETHANDLE m_id;
 };
-#ifdef USE_BOOST
 typedef boost::shared_ptr<server>  server_ptr;
-#else
-typedef std::shared_ptr<server>  server_ptr;
-#endif
-
-
 
 inline NETHANDLE server::get_id()
 {
@@ -60,5 +97,6 @@ inline void server::disconnect_clients()
 {
 	client_manager_singleton->pop_server_clients(get_id());
 }
+
 
 #endif
