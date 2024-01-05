45b09e4ad35a3dfc86ee49312822e7e2eb4a7c5a
zhichao281@163.com <a6808131>
2024-01-05 17:05:05 +0800

    升级了rtmp推理

--- ABLMediaServer/NetClientSendRtmp.h
+++ ABLMediaServer/NetClientSendRtmp.h
@@ -29,46 +29,46 @@ class CNetClientSendRtmp : public CNetRevcBase
 {
 public:
 	CNetClientSendRtmp(NETHANDLE hServer, NETHANDLE hClient, char* szIP, unsigned short nPort, char* szShareMediaURL);
-   ~CNetClientSendRtmp() ;
+	~CNetClientSendRtmp();
 
-   uint32_t       nVideoDTS ;
-   uint32_t       nAudioDTS;
-   uint32_t       nWriteRet;
-   volatile  int  nWriteErrorCount;
-   char           szRtmpName[256];
+	uint32_t       nVideoDTS;
+	uint32_t       nAudioDTS;
+	uint32_t       nWriteRet;
+	volatile  int  nWriteErrorCount;
+	char           szRtmpName[string_length_1024];
 
-   virtual int InputNetData(NETHANDLE nServerHandle, NETHANDLE nClientHandle, uint8_t* pData, uint32_t nDataLength, void* address) ;
-   virtual int ProcessNetData();
+	virtual int InputNetData(NETHANDLE nServerHandle, NETHANDLE nClientHandle, uint8_t* pData, uint32_t nDataLength, void* address);
+	virtual int ProcessNetData();
 
-   virtual int PushVideo(uint8_t* pVideoData, uint32_t nDataLength, char* szVideoCodec) ;//塞入视频数据
-   virtual int PushAudio(uint8_t* pAudioData, uint32_t nDataLength, char* szAudioCodec, int nChannels, int SampleRate) ;//塞入音频数据
-   virtual int SendVideo();//发送视频数据
-   virtual int SendAudio();//发送音频数据
-   virtual int SendFirstRequst();//发送第一个请求
-   virtual bool RequestM3u8File();//请求m3u8文件
+	virtual int PushVideo(uint8_t* pVideoData, uint32_t nDataLength, char* szVideoCodec);//塞入视频数据
+	virtual int PushAudio(uint8_t* pAudioData, uint32_t nDataLength, char* szAudioCodec, int nChannels, int SampleRate);//塞入音频数据
+	virtual int SendVideo();//发送视频数据
+	virtual int SendAudio();//发送音频数据
+	virtual int SendFirstRequst();//发送第一个请求
+	virtual bool RequestM3u8File();//请求m3u8文件
 
-   struct rtmp_client_handler_t handler;
-   rtmp_client_t*               rtmp;
-   volatile bool                bCheckRtspVersionFlag;
+	struct rtmp_client_handler_t handler;
+	rtmp_client_t* rtmp;
+	volatile bool                bCheckRtspVersionFlag;
 
-   int                          nAsyncAudioStamp;
+	int                          nAsyncAudioStamp;
 
-   flv_muxer_t*                 flvMuxer;
-   char                         szURL[512];
+	flv_muxer_t* flvMuxer;
+	char                         szURL[string_length_2048];
 
-   volatile bool                bDeleteRtmpPushH265Flag; //因为推rtmp265被删除标志 
+	volatile bool                bDeleteRtmpPushH265Flag; //因为推rtmp265被删除标志 
 
-   volatile bool                bAddMediaSourceFlag;//是否加入媒体库
-   int                          nRtmpState;        //rtmp状态 
-   int                          nRtmpState3Count; //状态3的次数 
+	volatile bool                bAddMediaSourceFlag;//是否加入媒体库
+	int                          nRtmpState;        //rtmp状态 
+	int                          nRtmpState3Count; //状态3的次数 
 
 #ifdef  WriteFlvFileByDebug
-   void*                        s_flv;
+	void* s_flv;
 #endif
 
 #ifdef  WriteFlvToEsFileFlagSend
-   FILE*                      fWriteVideo;
-   FILE*                      fWriteAudio;
+	FILE* fWriteVideo;
+	FILE* fWriteAudio;
 #endif
 };
 
