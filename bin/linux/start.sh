7f6bbcbad29d01a35c22d2ff0075e30e17aa9460
zhichao281@163.com <a6808131>
2024-04-10 17:21:11 +0800

    修改了配置文件

--- bin/linux/start.sh
+++ bin/linux/start.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # 设置 LD_LIBRARY_PATH 为当前路径
 export LD_LIBRARY_PATH="$SCRIPT_DIR"
 
+# 添加当前目录及其子目录的读写权限
+chmod -R +rw "$SCRIPT_DIR"
+
 # 切换到 ABLServer 可执行程序所在目录（假设在当前目录下）
 cd "$SCRIPT_DIR"
 
